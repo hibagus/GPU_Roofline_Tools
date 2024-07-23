a5ebefa5a7045cbe96ca0aabc18f89b72fd69a15
Bagus Hanindhito <git@bagus.my.id>
2024-07-23 21:03:17 +0000

    modify precision type

--- include/GPU_Roofline_Tools/utils/common/ptype.h
+++ include/GPU_Roofline_Tools/utils/common/ptype.h
@@ -2,10 +2,9 @@
 
 enum ptype 
 {
-    BF16V, // Vector-bound Operation on BF16
-    FP16V, // Vector-bound Operation on FP16
-    FP32V, // Vector-bound Operation on FP32
-    FP64V, // Vector-bound Operation on FP64
-    FP16M, // Matrix-bound Operation on FP16
-    FP32M  // Matrix-bound Operation on FP32
+    INT8, // INT8
+    BF16, // BF16
+    FP16, // FP16
+    FP32, // FP32
+    FP64, // FP64
 };
