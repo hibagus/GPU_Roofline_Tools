07f9e3b437d5a4bf42c51075274438a66ed19294
Bagus Hanindhito <git@bagus.my.id>
2024-07-23 21:03:39 +0000

    add bfloat16 vector ops

--- include/GPU_Roofline_Tools/launcher/rocm/kernel_launch_vector.hip.h
+++ include/GPU_Roofline_Tools/launcher/rocm/kernel_launch_vector.hip.h
@@ -5,6 +5,7 @@
 template<typename TCompute>
 inline metrics kernel_launch_vector(uint32_t n_wavefront, uint32_t n_workgroup, uint32_t n_loop, optype op);
 
+metrics kernel_launch_vector_bf16(uint32_t n_wavefront, uint32_t n_workgroup, uint32_t n_loop, optype op);
 metrics kernel_launch_vector_fp16(uint32_t n_wavefront, uint32_t n_workgroup, uint32_t n_loop, optype op);
 metrics kernel_launch_vector_fp32(uint32_t n_wavefront, uint32_t n_workgroup, uint32_t n_loop, optype op);
 metrics kernel_launch_vector_fp64(uint32_t n_wavefront, uint32_t n_workgroup, uint32_t n_loop, optype op);
