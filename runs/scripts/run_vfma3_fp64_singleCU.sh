a61683fab5fd732c941090018950ff41e080cf75
Bagus Hanindhito <git@bagus.my.id>
2024-07-29 16:42:15 +0000

    add some scripts

---
+++ runs/scripts/run_vfma3_fp64_singleCU.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+../../bin/amd_mi300x_bench \
+    --device 0 \
+    --operations V_FMA3 \
+    --vector-data-type fp64 \
+    --min-wavefront 1 \
+    --max-wavefront 16 \
+    --step-wavefront 1 \
+    --min-workgroup 1 \
+    --max-workgroup 1 \
+    --step-workgroup 1 \
+    2>&1 | tee ../outputs/run_vfma3_fp64_singleCU.out
