3a12fc65651d81ba3e6c1fb0e5ed0e4d274c9cc1
Bagus Hanindhito <git@bagus.my.id>
2025-03-07 11:59:50 +0000

    run script modification for vfma3 fp32

--- runs/scripts/run_vfma3_fp32_gpuWide.sh
+++ runs/scripts/run_vfma3_fp32_gpuWide.sh
@@ -4,10 +4,10 @@
     --device 0 \
     --operations V_FMA3 \
     --vector-data-type fp32 \
-    --min-wavefront 1 \
+    --min-wavefront 16 \
     --max-wavefront 16 \
     --step-wavefront 1 \
     --min-workgroup 1 \
     --max-workgroup 304 \
     --step-workgroup 1 \
-    2>&1 | tee ../outputs/run_fma3_fp32_gpuWide.out
+    2>&1 | tee ../outputs/run_vfma3_fp32_gpuWide.out
