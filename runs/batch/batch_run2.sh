fab14fb4cc4055ac70a0cb4e797d93b4a11cc8b3
Bagus Hanindhito <git@bagus.my.id>
2025-03-07 12:04:16 +0000

    add batch_run2.sh

---
+++ runs/batch/batch_run2.sh
@@ -0,0 +1,20 @@
+./run_workload_monitor.sh "../../monitors/scripts/amd_mi300x_power_clock_temp.sh 0 1 run_vfma3_fp32_gpuWide"   "../scripts/run_vfma3_fp32_gpuWide.sh"
+./run_workload_monitor.sh "../../monitors/scripts/amd_mi300x_power_clock_temp.sh 0 1 run_mwmma_bf16-fp32_singleCU"   "../scripts/run_mwmma_bf16-fp32_singleCU.sh"
+./run_workload_monitor.sh "../../monitors/scripts/amd_mi300x_power_clock_temp.sh 0 1 run_mwmma_bf8-fp32_singleCU"   "../scripts/run_mwmma_bf8-fp32_singleCU.sh"
+./run_workload_monitor.sh "../../monitors/scripts/amd_mi300x_power_clock_temp.sh 0 1 run_mwmma_fp16-fp32_gpuWide"   "../scripts/run_mwmma_fp16-fp32_gpuWide.sh"
+./run_workload_monitor.sh "../../monitors/scripts/amd_mi300x_power_clock_temp.sh 0 1 run_mwmma_fp16-fp32_singleCU"   "../scripts/run_mwmma_fp16-fp32_singleCU.sh"
+./run_workload_monitor.sh "../../monitors/scripts/amd_mi300x_power_clock_temp.sh 0 1 run_mwmma_fp32-fp32_gpuWide"   "../scripts/run_mwmma_fp32-fp32_gpuWide.sh"
+./run_workload_monitor.sh "../../monitors/scripts/amd_mi300x_power_clock_temp.sh 0 1 run_mwmma_fp32-fp32_singleCU"   "../scripts/run_mwmma_fp32-fp32_singleCU.sh"
+./run_workload_monitor.sh "../../monitors/scripts/amd_mi300x_power_clock_temp.sh 0 1 run_mwmma_fp64-fp64_gpuWide"   "../scripts/run_mwmma_fp64-fp64_gpuWide.sh"
+./run_workload_monitor.sh "../../monitors/scripts/amd_mi300x_power_clock_temp.sh 0 1 run_mwmma_fp64-fp64_singleCU"   "../scripts/run_mwmma_fp64-fp64_singleCU.sh"
+./run_workload_monitor.sh "../../monitors/scripts/amd_mi300x_power_clock_temp.sh 0 1 run_mwmma_fp8-fp32_gpuWide"   "../scripts/run_mwmma_fp8-fp32_gpuWide.sh"
+./run_workload_monitor.sh "../../monitors/scripts/amd_mi300x_power_clock_temp.sh 0 1 run_mwmma_fp8-fp32_singleCU"   "../scripts/run_mwmma_fp8-fp32_singleCU.sh"
+./run_workload_monitor.sh "../../monitors/scripts/amd_mi300x_power_clock_temp.sh 0 1 run_mwmma_int8-int32_gpuWide"   "../scripts/run_mwmma_int8-int32_gpuWide.sh"
+./run_workload_monitor.sh "../../monitors/scripts/amd_mi300x_power_clock_temp.sh 0 1 run_mwmma_int8-int32_singleC"   "../scripts/run_mwmma_int8-int32_singleCU.sh"
+./run_workload_monitor.sh "../../monitors/scripts/amd_mi300x_power_clock_temp.sh 0 1 run_mwmma_tf32-fp32_gpuWide"   "../scripts/run_mwmma_tf32-fp32_gpuWide.sh"
+./run_workload_monitor.sh "../../monitors/scripts/amd_mi300x_power_clock_temp.sh 0 1 run_mwmma_tf32-fp32_singleCU"   "../scripts/run_mwmma_tf32-fp32_singleCU.sh"
+
+
+
+
+
