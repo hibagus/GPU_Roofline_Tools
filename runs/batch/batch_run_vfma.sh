b5b3c818098be1a7a3451d56dd9cd34381b0642d
Bagus Hanindhito <git@bagus.my.id>
2025-03-24 17:16:56 +0000

    re-arranging batch runscript for leftover run

---
+++ runs/batch/batch_run_vfma.sh
@@ -0,0 +1,22 @@
+./run_workload_monitor.sh "../../monitors/scripts/amd_mi300x_power_clock_temp.sh 0 1 run_vfma3_fp16_gpuWide"   "../scripts/run_vfma3_fp16_gpuWide.sh"
+./run_workload_monitor.sh "../../monitors/scripts/amd_mi300x_power_clock_temp.sh 0 1 run_vfma3_fp16_singleCU"  "../scripts/run_vfma3_fp16_singleCU.sh"
+./run_workload_monitor.sh "../../monitors/scripts/amd_mi300x_power_clock_temp.sh 0 1 run_vfma3_fp32_gpuWide"   "../scripts/run_vfma3_fp32_gpuWide.sh"
+./run_workload_monitor.sh "../../monitors/scripts/amd_mi300x_power_clock_temp.sh 0 1 run_vfma3_fp32_singleCU"  "../scripts/run_vfma3_fp32_singleCU.sh"
+./run_workload_monitor.sh "../../monitors/scripts/amd_mi300x_power_clock_temp.sh 0 1 run_vfma3_fp64_gpuWide"   "../scripts/run_vfma3_fp64_gpuWide.sh"
+./run_workload_monitor.sh "../../monitors/scripts/amd_mi300x_power_clock_temp.sh 0 1 run_vfma3_fp64_singleCU"  "../scripts/run_vfma3_fp64_singleCU.sh"
+./run_workload_monitor.sh "../../monitors/scripts/amd_mi300x_power_clock_temp.sh 0 1 run_vfma2_fp16_gpuWide"   "../scripts/run_vfma2_fp16_gpuWide.sh"
+./run_workload_monitor.sh "../../monitors/scripts/amd_mi300x_power_clock_temp.sh 0 1 run_vfma2_fp16_singleCU"  "../scripts/run_vfma2_fp16_singleCU.sh"
+./run_workload_monitor.sh "../../monitors/scripts/amd_mi300x_power_clock_temp.sh 0 1 run_vfma2_fp32_gpuWide"   "../scripts/run_vfma2_fp32_gpuWide.sh"
+./run_workload_monitor.sh "../../monitors/scripts/amd_mi300x_power_clock_temp.sh 0 1 run_vfma2_fp32_singleCU"  "../scripts/run_vfma2_fp32_singleCU.sh"
+./run_workload_monitor.sh "../../monitors/scripts/amd_mi300x_power_clock_temp.sh 0 1 run_vfma2_fp64_gpuWide"   "../scripts/run_vfma2_fp64_gpuWide.sh"
+./run_workload_monitor.sh "../../monitors/scripts/amd_mi300x_power_clock_temp.sh 0 1 run_vfma2_fp64_singleCU"  "../scripts/run_vfma2_fp64_singleCU.sh"
+./run_workload_monitor.sh "../../monitors/scripts/amd_mi300x_power_clock_temp.sh 0 1 run_vfma1_fp16_gpuWide"   "../scripts/run_vfma1_fp16_gpuWide.sh"
+./run_workload_monitor.sh "../../monitors/scripts/amd_mi300x_power_clock_temp.sh 0 1 run_vfma1_fp16_singleCU"  "../scripts/run_vfma1_fp16_singleCU.sh"
+./run_workload_monitor.sh "../../monitors/scripts/amd_mi300x_power_clock_temp.sh 0 1 run_vfma1_fp32_gpuWide"   "../scripts/run_vfma1_fp32_gpuWide.sh"
+./run_workload_monitor.sh "../../monitors/scripts/amd_mi300x_power_clock_temp.sh 0 1 run_vfma1_fp32_singleCU"  "../scripts/run_vfma1_fp32_singleCU.sh"
+./run_workload_monitor.sh "../../monitors/scripts/amd_mi300x_power_clock_temp.sh 0 1 run_vfma1_fp64_gpuWide"   "../scripts/run_vfma1_fp64_gpuWide.sh"
+./run_workload_monitor.sh "../../monitors/scripts/amd_mi300x_power_clock_temp.sh 0 1 run_vfma1_fp64_singleCU"  "../scripts/run_vfma1_fp64_singleCU.sh"
+
+
+
+
