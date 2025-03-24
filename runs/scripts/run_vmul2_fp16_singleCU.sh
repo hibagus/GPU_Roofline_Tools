293978e45221ba37fc8398b0311840cb63182df7
Bagus Hanindhito <git@bagus.my.id>
2025-03-24 15:56:58 +0000

    add vector-vector multiplication and vector-scalar addition runscripts

---
+++ runs/scripts/run_vmul2_fp16_singleCU.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+../../bin/amd_mi300x_bench \
+    --device 0 \
+    --operations V_MUL2 \
+    --vector-data-type fp16 \
+    --min-wavefront 1 \
+    --max-wavefront 16 \
+    --step-wavefront 1 \
+    --min-workgroup 1 \
+    --max-workgroup 1 \
+    --step-workgroup 1 \
+    2>&1 | tee ../outputs/run_vmul2_fp16_singleCU.out
