3f8dd51b58d6aab353181bde4d22c21aa11a0050
Bagus Hanindhito <git@bagus.my.id>
2025-03-30 00:22:48 +0000

    fix vadd1 and vadd2 runscript for gpuwide

--- runs/scripts/run_vadd2_fp16_gpuWide.sh
+++ runs/scripts/run_vadd2_fp16_gpuWide.sh
@@ -7,7 +7,7 @@
     --min-wavefront 16 \
     --max-wavefront 16 \
     --step-wavefront 1 \
-    --min-workgroup 304 \
+    --min-workgroup 1 \
     --max-workgroup 304 \
     --step-workgroup 1 \
     2>&1 | tee ../outputs/run_vadd2_fp16_gpuWide.out
