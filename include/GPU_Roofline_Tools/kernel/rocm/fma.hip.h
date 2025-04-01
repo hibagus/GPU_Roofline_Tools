1289e3ef5c290235cf4ec707026ccbfec9e8a42c
Bagus Hanindhito <git@bagus.my.id>
2025-04-01 12:17:46 +0000

    modify fma1 to use two scalars

--- include/GPU_Roofline_Tools/kernel/rocm/fma.hip.h
+++ include/GPU_Roofline_Tools/kernel/rocm/fma.hip.h
@@ -98,3 +98,34 @@ __global__ void fma(TCompute *buf_A, const TCompute B, uint64_t *dev_n_clockCoun
 
     if(lane_id_x==0){dev_n_clockCount[wavefront_id] = end_time-start_time;}
 }
+
+template<typename TCompute>
+__global__ void fma(TCompute *buf_A, const TCompute B, const TCompute C, uint64_t *dev_n_clockCount, uint32_t wf_sz)
+{
+    // Global Index (NDRange-level)
+    const uint32_t thread_id     = hipBlockDim_x * hipBlockIdx_x + hipThreadIdx_x;
+    const uint32_t wavefront_id  = thread_id / wf_sz;
+
+    // Local Index (Workgroup-level)
+    // This is to determine whether a thread is the 0th thread in a waveform (i.e., lane 0)
+    const uint32_t lane_id_x = hipThreadIdx_x & (wf_sz - 1);
+
+    uint64_t start_time;
+    uint64_t end_time;
+
+    // Load operand from Memory to Register
+    TCompute a = buf_A[thread_id];
+
+    start_time = clock64();
+    #pragma unroll
+    for(int iter=0; iter < NUM_LOOPS; iter++)
+    {
+        a = a * B + C;
+    }
+    end_time = clock64();
+
+    // Store back the result to memory
+    buf_A[thread_id] = a;
+
+    if(lane_id_x==0){dev_n_clockCount[wavefront_id] = end_time-start_time;}
+}
